0e2342d7ef0fbed1931326a15bdcd5f4fa659ac0
Hedonista17 <andreaynereapiza@gmail.com>
2023-02-11 11:15:05 +0000

    primer intento todolist

---
+++ index.js
@@ -0,0 +1,13 @@
+const ToDoForm =document.querySelector("#todo")
+
+
+ToDoForm.addEventListener("change", event =>{
+console.log(event.target.value);
+});
+
+ToDoForm.addEventListener("submit", event =>{
+    event.preventDefault();
+    });
+const creatTodo = todo =>{
+
+};
